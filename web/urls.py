cce918cdca3e98ed8e804155b902d111e51b35ed
Ayisha-SS <13aiysh246@gmail.com>
2024-07-01 01:21:36 +0530

    update

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from web.views import index
+from web.views import index,details
 
 app_name = 'web'
 
 
 urlpatterns = [
     path('',index,name="index"),
+    path('details/',details,name="details")
 ]
